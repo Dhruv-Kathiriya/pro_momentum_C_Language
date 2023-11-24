15a6af8a11dbda7a8776cca83b0f43d667453aab
Dhruv-Kathiriya <150034575+Dhruv-Kathiriya@users.noreply.github.com>
2023-11-24 10:53:23 +0530

    Update bill.c
    edit and clear output display and turbooc++ program

--- bill.c
+++ bill.c
@@ -1,25 +1,26 @@
 #include<stdio.h>
-
-main(){
-	
-	double unit,s,cost,bill;
-	
-	printf("enter unit :");
-	scanf("%lf",&unit);
-	
-	if(unit<=50){
-		s=unit*0.50;
-	}else if(unit<=150){
-		s=(unit-50)*0.75+25;
-	}else if(unit<=250){
-		s=(unit-150)*1.25+100;
-	}else {
-		s=(unit-250)*1.50+225;
-	}
-	printf("total = %.2lf\n",s);
-	
-	cost=s*20/100;
-	bill=s+cost;
-	
-	printf("bill = %.2lf\n",bill);
+#include<conio.h>
+main()
+{
+    
+    double unit,cost,surcharge,bill;
+	clrscr();
+    printf("enter unit :");
+    scanf("%lf",&unit);
+     
+      if(unit<=50){
+        surcharge= unit*0.50;
+    }else if(unit<=150){
+       surcharge = (unit-50)*0.75+25;
+    }else if(unit<=250){
+       surcharge = (unit-150)*1.25+100;
+    }else{
+       surcharge= (unit-250)*1.50+225;
+    }
+        printf("Total = %.2lf\n",surcharge);
+        cost = surcharge*20/100;
+        bill = surcharge+cost;
+        printf("bill = %.2lf\n",bill);
+        getch();
+        
 }
